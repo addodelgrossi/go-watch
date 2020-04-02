ae37825041f2ecac92c39b353dc36597091c4e3b
Addo <addo.delgrossi@gmail.com>
2020-04-02 11:20:26 -0300

    Add basic features for the new watch command
    It allows to watch the program output change over time. By default, the
    specified command runs every 2 seconds, and watch will run until
    interrupted. You can use some options to customize the default way:
    interval, timeout, quiet and halt.
    
    It was based on https://github.com/tj/watch.
    
    Co-Authored-By: Leonardo Comelli <leonardo.comelli@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# go-watch
-watch(1) periodically executes the given command - useful for auto-testing, auto-building, auto-anything, port for tj/watch
+# Go Watch
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+const (
+	version = "0.3.1"
+)
+
+var (
+	interval int
+	timeout  int
+	quiet    bool
+	halt     bool
+	help     bool
+)
+
+func main() {
+	flag.IntVar(&interval, "interval", 2, "interval in seconds defaulting to 1")
+	flag.IntVar(&timeout, "timeout", -1, "duration until the program ends")
+	flag.BoolVar(&quiet, "quiet", false, "only output stderr")
+	flag.BoolVar(&halt, "halt", false, "halt on failure")
+	flag.BoolVar(&help, "help", false, "output this help information")
+
+	if len(os.Args) <= 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	flag.Parse()
+
+	val := strings.Join(flag.Args(), " ")
+	commands := []string{"-c", val}
+
+	fmt.Print("\033[s") // cursor mark
+
+	done := make(chan bool)
+
+	go func() {
+		finishAt := time.Now().Add(time.Duration(timeout) * time.Second)
+		for {
+
+			if timeout > 0 && time.Now().After(finishAt) {
+				close(done)
+			}
+
+			cmd := exec.Command("sh", commands...)
+			out, err := cmd.CombinedOutput()
+
+			fmt.Print("\033[u\033[K") // reset cursor
+
+			if !quiet || err != nil {
+				fmt.Printf("%s", out)
+			}
+
+			if halt && err != nil {
+				exitCode := 124
+				if xerr, ok := err.(*exec.ExitError); ok {
+					exitCode = xerr.ExitCode()
+				}
+				os.Exit(exitCode)
+			}
+
+			time.Sleep(time.Duration(interval) * time.Second)
+		}
+	}()
+
+	<-done
+}
